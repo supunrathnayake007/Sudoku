a3483c6b8093fe915364afafeae364c58d4a9c54
Supun Rathnayake <supun.rathnayake007@gmail.com>
2023-11-14 09:03:20 +0530

    improve the code added commets to better understand and function names changed

--- functions.py
+++ functions.py
@@ -4,6 +4,8 @@ import FunctionSupport.basic_grid_control
 def solve_sudoku(my_list: list, testing_mode: bool) -> list:
 
     possible_values = FunctionSupport.basic_grid_control.new_possible_values_grid()
+    possible_values = remove_unnecessary_possible_values(
+        my_list, possible_values)
 
     while 1 == 1:
         before_count = _count_possibleValues(possible_values)
@@ -29,42 +31,49 @@ def solve_sudoku(my_list: list, testing_mode: bool) -> list:
     return my_list
 
 
-def guess_possibleValues(main_list: list, row: int, col: int, possible_values: list) -> list:
-    if main_list[row][col] > 0:
-        return []
-    # possible_values = [1,2,3,4,5,6,7,8,9]
-    # remove values in selected row
-    for i in range(0, 9):
-        if i == col:
-            continue
-        for j in possible_values:
-            if j == main_list[row][i]:
-                possible_values.remove(j)
+def remove_unnecessary_possible_values(main_list: list, possible_values: list) -> list:
+    # old name - guess_possibleValues
+    # this method good to call when after value added to the main grid or after new possible value list created
+    # in this method we are going to clean the possible value list according to main grid
+    # like if there value in the main grid then that value doesn't need to be possible value in the entire row column and 3x3 cell
+    # its like a cleaning remaining unnecessary possible values
 
-    # remove values in selected column
-    for i in range(0, 9):
-        if i == row:
-            continue
-        for j in possible_values:
-            if j == main_list[i][col]:
-                possible_values.remove(j)
+    for row in range(0, 9):
+        for col in range(0, 9):
+            # pv_list[row][col] = guess_possibleValues(
+            #     main_list, row, col, pv_list[row][col])
 
-    def get_range(index: int) -> list:
-        if index < 3 and index > -1:
-            return [0, 1, 2]
-        elif index < 6 and index > 2:
-            return [3, 4, 5]
-        else:
-            return [6, 7, 8]
+            # in here if there value in main grid then we gonna skip that cell
+            # because it already have a value no need any checking
+            # to think - we also can remove all the possible values, may be assign 0
+            if main_list[row][col] > 0:
+                continue
 
-    # remove values in 3x3 grid
-    row_range = get_range(row)
-    col_range = get_range(col)
-    for i in row_range:
-        for j in col_range:
-            for k in possible_values:
-                if k == main_list[i][j]:
-                    possible_values.remove(k)
+            # this check the entire column and remove unnecessary possible values
+            for i in range(0, 9):  # i is column index
+                if i == col:  # this prevent checking its own cell - improve this hard to understand
+                    # this might not need, not sure , keeping it seems no harm
+                    continue
+                for j in possible_values[row][col]:  # j is possible value
+                    if j == main_list[row][i]:
+                        possible_values[row][col].remove(j)
+
+            # this check the entire row and remove unnecessary possible values, work same like above
+            for i in range(0, 9):  # i is row index
+                if i == row:
+                    continue
+                for j in possible_values[row][col]:
+                    if j == main_list[i][col]:
+                        possible_values[row][col].remove(j)
+
+            # this check the entire 3x3 cell and remove unnecessary possible values
+            row_range = _get_range(row)
+            col_range = _get_range(col)
+            for i in row_range:
+                for j in col_range:
+                    for k in possible_values[row][col]:
+                        if k == main_list[i][j]:
+                            possible_values[row][col].remove(k)
 
     return possible_values
 
@@ -74,10 +83,10 @@ def removeIVF_pvTemplate(main_list: list, pv_list: list) -> list:
     # new name - filter_possible_values
 
     # this filter out possible values for selected cell,  it check the row , column and 3x3 cell for filter out
-    for row in range(0, 9):
-        for col in range(0, 9):
-            pv_list[row][col] = guess_possibleValues(
-                main_list, row, col, pv_list[row][col])
+    # for row in range(0, 9):
+    #     for col in range(0, 9):
+    #         pv_list[row][col] = guess_possibleValues(
+    #             main_list, row, col, pv_list[row][col])
 
     # description 1 - this is another major rule or strategy for set the values - when there possible value is uniq to its row , column or 3x3cel
     # even other possible values existed, then that uniq value is the value should be
@@ -102,45 +111,56 @@ def removeIVF_pvTemplate(main_list: list, pv_list: list) -> list:
                     if is_available:
                         break
 
+                # if selected possible value doesn't exist in the other cells on the row, remove other possible values in that cell
+                # that leave one possible values and that will be the certain value for the grid - [may improve need to understand]
+                # after selecting the certain value, possible value loop will brake here and not going further
                 if is_available == False:
                     pv_list[row][col] = [possible_value]
                     break
-                else:
-                    is_available = False
-                    for l in range(0, 9):
-                        if l == i:
+
+                # else - if possible value exist in the row, "is_available" value reset for false and proceed to next step
+                # which is gonna check column
+                is_available = False
+
+                # this is same like above loop but this checks the entire column instead the row.
+                # if there no possible value like the selected one "is_available" value remain false
+                # it means selected possible value is the certain value
+                for l in range(0, 9):
+                    if l == row:
+                        continue
+                    for m in pv_list[l][col]:
+                        if possible_value == m:
+                            is_available = True
+                            break
+                    if is_available:
+                        break
+
+                # this is like above line 108
+                if is_available == False:
+                    pv_list[row][col] = [possible_value]
+                    break
+
+                # above we check the row then column now we gonna check 3x3 cell
+                is_available = False
+                row_range = _get_range(row)
+                col_range = _get_range(col)
+
+                for l in row_range:
+                    for m in col_range:
+                        if l == row and m == col:
                             continue
-                        for m in pv_list[l][col]:
-                            if possible_value == m:
+                        for n in pv_list[l][m]:
+                            if possible_value == n:
                                 is_available = True
                                 break
                         if is_available:
                             break
+                    if is_available:
+                        break
 
                 if is_available == False:
                     pv_list[row][col] = [possible_value]
                     break
-                else:
-                    is_available = False
-                    row_range = _get_range(row)
-                    col_range = _get_range(col)
-
-                    for l in row_range:
-                        for m in col_range:
-                            if l == row and m == col:
-                                continue
-                            for n in pv_list[l][m]:
-                                if possible_value == n:
-                                    is_available = True
-                                    break
-                            if is_available:
-                                break
-                        if is_available:
-                            break
-
-                    if is_available == False:
-                        pv_list[row][col] = [possible_value]
-                        break
 
     # for i in range(0, 9):
     #     have_values = 0
@@ -174,7 +194,8 @@ def _insert_valuesWTOOPA(main_list: list, pv_list: list) -> list:
 
 
 def _get_range(index: int) -> list:
-    # give the given index of 3x3 grid range - improve the description
+    # give the range of given index for 3x3 grid
+    # this works well with row and column, coz there are both same
     if index < 3 and index > -1:
         return [0, 1, 2]
     elif index < 6 and index > 2:
